4620a80d9ec7f8e43c9c9e14395f11fe960319b1
daddybonbon <lawmac@optusnet.com.au>
2022-03-27 22:46:47 +1100

    updated slider images

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the ElonGates NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The ElonGates NFT Project';
+const welcome_p = 'What lies beyond ElonGates?';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
