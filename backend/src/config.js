5c6e667eb40740693704949e83ce740c1d9692aa
daddybonbon <lawmac@optusnet.com.au>
2022-03-28 20:43:35 +1100

    updat white list

--- backend/src/config.js
+++ backend/src/config.js
@@ -62,7 +62,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0xF1d5e6623E33C69321C20C39d278107A5B21d2CB"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = [""]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
